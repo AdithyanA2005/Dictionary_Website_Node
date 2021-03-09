b0989c4e46e2ee4bc26b96a608c3a69445fe2e18
Adithyan A <deepaadithyan56@gmail.com>
2021-03-09 23:16:38 +0530

    Layout Changed
    A small layout change was added by setting up basedir

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var pug = require('pug');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,15 +13,19 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('basedir', path.join(__dirname, ''))
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/jscss', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.locals.basedir = app.get('basedir');
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
