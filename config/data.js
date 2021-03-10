fdd89f5cefe744f2638cdcdbf57926380cdce625
Adithyan A <deepaadithyan56@gmail.com>
2021-03-10 22:20:38 +0530

    Data.json Converted To Data.js
    The data in JSON file was converted into JS Objects

---
+++ config/data.js
@@ -0,0 +1,252 @@
+let meaning = [
+  {
+    name: "accidial",
+    meaning: "dial someone's number on phone accidentally",
+  },
+  {
+    name: "agender",
+    meaning: "people do not identify as male or female",
+  },
+  {
+    name: "airball",
+    meaning: "completely miss the basket, rim, and backboard with a shot",
+  },
+  {
+    name: "automagically",
+    meaning: "a way that seems magical, especially by computer",
+  },
+  {
+    name: "awesomesauce",
+    meaning: "extremely good; excellent",
+  },
+  {
+    name: "bargainous",
+    meaning: "costing less than expected, cheap or relatively cheap",
+  },
+  {
+    name: "barista",
+    meaning:
+      "a person whose job involves preparing and serving different types of coffee",
+  },
+  {
+    name: "bedunged",
+    meaning:
+      "has been soiled with or covered in dung; very old or old-fashioned",
+  },
+  {
+    name: "binge watch",
+    meaning:
+      "watch multiple episodes of a television program in rapid succession",
+  },
+  {
+    name: "bitcoin",
+    meaning: "an online payment system that does not require an intermediary",
+  },
+  {
+    name: "bling",
+    meaning:
+      "jewellery, decoration, or clothing that attracts attention because looks expensive",
+  },
+  {
+    name: "blog",
+    meaning:
+      "regularly updated website or web page, typically one run by an individual or small group",
+  },
+  {
+    name: "bokeh",
+    meaning:
+      "visual quality of the out-of-focus areas of a photographic image, especially as rendered by a particular lens",
+  },
+  {
+    name: "bongga",
+    meaning: "extravagant, flamboyant, impressive, stylish, or excellent",
+  },
+  {
+    name: "brexit",
+    meaning:
+      "a term for the potential or hypothetical departure of the United Kingdom from the European Union",
+  },
+  {
+    name: "bromance",
+    meaning: "a close but non-sexual relationship between two men",
+  },
+  {
+    name: "buko",
+    meaning:
+      "coconut, coconut water; a person who is less than 5ft tall and really angry",
+  },
+  {
+    name: "butt dial",
+    meaning:
+      "inadvertently call on a mobile phone in one's rear pants pocket, as a result of pressure being accidentally applied to buttons on the phone",
+  },
+  {
+    name: "butt hurt",
+    meaning:
+      "inappropriately strong negative emotional response from a perceived personal insult, strong feelings of shame",
+  },
+  {
+    name: "buzzkill",
+    meaning: "person or thing that has a depressing effect",
+  },
+  {
+    name: "catastrophize",
+    meaning: "present a situation as worse than it is",
+  },
+  {
+    name: "cheeseball",
+    meaning: "lacking taste, style, or originality",
+  },
+  {
+    name: "chillax",
+    meaning: "calm down and relax",
+  },
+  {
+    name: "clickbait",
+    meaning: "online content to attract more visitors to a particular website",
+  },
+  {
+    name: "conlang",
+    meaning: "an invented language intended for human communication",
+  },
+  {
+    name: "cool hunter",
+    meaning: "one who predicts new styles and trends",
+  },
+  {
+    name: "crema",
+    meaning: "a brownish foam that forms on the top of freshly made coffee",
+  },
+  {
+    name: "crowdfund",
+    meaning:
+      "fund a project from lots of people who usually donate a small amount",
+  },
+  {
+    name: "crunk",
+    meaning: "very excited or full of energy",
+  },
+  {
+    name: "dark web",
+    meaning:
+      "part of the Internet intentionally hidden from search engines with masked IP addresses, usually illegal content",
+  },
+  {
+    name: "droolworthy",
+    meaning: "extremely attractive or desirable",
+  },
+  {
+    name: "ear tickler",
+    meaning:
+      "someone who pleases other people with paying lot of compliments or flattering",
+  },
+  {
+    name: "echo chamber",
+    meaning:
+      "an environment in which a person encounters only beliefs or opinions that coincide with their own, especially in social media",
+  },
+  {
+    name: "eggcorn",
+    meaning:
+      "word or phrase that results from a mishearing because of similar sounds",
+  },
+  {
+    name: "emoji",
+    meaning: "a small digital image or icon used to express an idea, emotion",
+  },
+  {
+    name: "facepalm",
+    meaning:
+      "cover one's face with the hand as an expression of embarrassment, dismay, or exasperation",
+  },
+  {
+    name: "facerape",
+    meaning:
+      "hack someone's social-media profile in order to fiddle with its personal information or to post fake status updates",
+  },
+  {
+    name: "fast fashionan",
+    meaning:
+      "approach that emphasizes making fashion clothing trends quickly and cheaply available to consumers",
+  },
+  {
+    name: "flash mob",
+    meaning:
+      "brief public gathering for a common purpose, typically organized by the Internet or social media ",
+  },
+  {
+    name: "fomo",
+    meaning:
+      "the fear of missing out, the anxiety that an exciting or interesting event may currently be happening elsewhere",
+  },
+  {
+    name: "foodie",
+    meaning: "a person with a particular interest in food; a gourmet",
+  },
+  {
+    name: "frankenfood",
+    meaning: "genetically modified food",
+  },
+  {
+    name: "freegan",
+    meaning:
+      "a person who rejects consumerism and seeks to help the environment by reducing waste",
+  },
+  {
+    name: "frenemy",
+    meaning: "a person with whom one is friendly despite a fundamental dislike",
+  },
+  {
+    name: "frousin",
+    meaning:
+      "a friend that is a cousin or a cousin that is a friend, very close relationship as family members",
+  },
+  {
+    name: "froyo",
+    meaning:
+      "frozen yogurt, a slightly sour thick food made from milk, and often sugar and fruit",
+  },
+  {
+    name: "gig economy",
+    meaning:
+      "a labor market characterized by short-term contracts or work as opposed to permanent jobs",
+  },
+  {
+    name: "ginger",
+    meaning: "a person with red hair",
+  },
+  {
+    name: "glamping",
+    meaning:
+      "outdoor camping with amenities and comforts, such as beds and electricity, not usually used when camping",
+  },
+  {
+    name: "hangry",
+    meaning: "bad-tempered or irritable as a result of hunge",
+  },
+  {
+    name: "homeshoring",
+    meaning: "moving jobs to employees' homes from offices or factories",
+  },
+  {
+    name: "humblebrag",
+    meaning:
+      "make a seemingly modest or self-critical to draw attention to something of which one is proud",
+  },
+  {
+    name: "hyperconnected",
+    meaning:
+      "characterized by the widespread or habitual use of devices that have Internet connectivity",
+  },
+  {
+    name: "hypermiling",
+    meaning: "altering a car to maximize its fuel economy",
+  },
+  {
+    name: "idiocracy",
+    meaning:
+      "an act or actions that come from ideas or beliefs that are not smart",
+  },
+];
+
+module.exports = meaning;
