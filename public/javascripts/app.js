70b96921ac47806c327cbf09c612f54905c3c842
Adithyan A <deepaadithyan56@gmail.com>
2021-03-10 22:19:51 +0530

    Custom JS Updated
    The code was beautified and new function to make the older search function noncase sensitive and special character sensitive was added

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,28 +1,62 @@
-function search_word() {
-    let li = '';
-    let text = '';
-    let pointer = 0;
+function replace_special(data_text) {
+  data_text = data_text.replace(" ", "");
 
-    const input = document.getElementById("word_entry");
-    const filter = input.value.toUpperCase();
-    const table = document.getElementById("table");
-    const ul = table.getElementsByTagName("ul");
-    const length = ul.length;
+  // TODO: Make this fucntion respond to all below commented lines
 
-    for(pointer; pointer < length; pointer++) {
-        li = ul[pointer].getElementsByTagName('li')[0];        
-
-        if(li) {
-            text = li.textContent || li.innerText;
-
-            if(text.toUpperCase().indexOf(filter) > -1) {
-                ul[pointer].style.display = '';
-            }
-            else {
-                ul[pointer].style.display = 'none';
-            }
-        }
-    }
+  // data_text = data_text.replace('!', '');
+  // data_text = data_text.replace('@', '');
+  // data_text = data_text.replace('#', '');
+  // data_text = data_text.replace('$', '');
+  // data_text = data_text.replace('%', '');
+  // data_text = data_text.replace('^', '');
+  // data_text = data_text.replace('&', '');
+  // data_text = data_text.replace('*', '');
+  // data_text = data_text.replace('(', '');
+  // data_text = data_text.replace(')', '');
+  // data_text = data_text.replace('-', '');
+  // data_text = data_text.replace('_', '');
+  // data_text = data_text.replace('=', '');
+  // data_text = data_text.replace('+', '');
+  // data_text = data_text.replace('[', '');
+  // data_text = data_text.replace(']', '');
+  // data_text = data_text.replace('{', '');
+  // data_text = data_text.replace('}', '');
+  // data_text = data_text.replace(';', '');
+  // data_text = data_text.replace(':', '');
+  // data_text = data_text.replace("'", '');
+  // data_text = data_text.replace('"', '');
+  // data_text = data_text.replace('<', '');
+  // data_text = data_text.replace(',', '');
+  // data_text = data_text.replace('>', '');
+  // data_text = data_text.replace('.', '');
+  // data_text = data_text.replace('/', '');
+  // data_text = data_text.replace('?', '');
+  return data_text;
 }
 
+function search_word() {
+  let li = "";
+  let text = "";
+  let pointer = 0;
 
+  const input = document.getElementById("word_entry");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("table");
+  const ul = table.getElementsByTagName("ul");
+  const length = ul.length;
+
+  for (pointer; pointer < length; pointer++) {
+    li = ul[pointer].getElementsByTagName("li")[0];
+
+    if (li) {
+      data_text = li.textContent || li.innerText;
+      text = replace_special(data_text);
+
+      if (text.toUpperCase().indexOf(filter) > -1) {
+        ul[pointer].style.display = "";
+      } else {
+        ul[pointer].style.display = "none";
+      }
+    }
+  }
+}
